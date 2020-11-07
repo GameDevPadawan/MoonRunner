201bb44e92809b113197adc1512c896d5472f7ac
GameDevPadawan <>
2020-11-07 16:50:36 -0500

    Added URP

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+	public float speed = 6.0F;
+
+	private Vector2 moveDirection = Vector2.zero;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		// Use input up and down for direction, multiplied by speed
+		moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed * Time.deltaTime;
+		transform.position += (Vector3)moveDirection;
+	}
+}
