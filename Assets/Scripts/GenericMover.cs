e0f20c4b757f11671e21040c063d891e491c4108
GameDevPadawan <>
2020-11-07 20:09:53 -0500

    Add player movement

---
+++ Assets/Scripts/GenericMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericMover
+{
+    private Transform t;
+
+    public GenericMover(Transform gameObjectTransform)
+    {
+        t = gameObjectTransform;
+    }
+
+    public void MoveInDirection(Vector3 direction, float speed, float deltaTime)
+    {
+        t.position += direction * speed * deltaTime;
+    }
+
+    public void MoveTowardsTarget(Vector3 currentPosition, Vector3 targetPosition, float speed, float deltaTime)
+    {
+        t.position = Vector3.MoveTowards(currentPosition, targetPosition, speed * deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+	public float speed = 6.0F;
+	private GenericMover mover;
+
+	void Awake()
+	{
+		mover = new GenericMover(this.transform);
+	}
+
+	void Update()
+	{
+		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		// normalize the vector so we do no move faster when moving diagonally
+		// Simple explanation here: http://answers.unity.com/answers/1291321/view.html
+		moveDirection.Normalize();
+		mover.MoveInDirection(moveDirection, speed, Time.deltaTime);
+	}
+}
